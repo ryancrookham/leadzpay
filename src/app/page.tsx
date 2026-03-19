b0a71c8913dd3048d8be2e4a38ff62fc69ffc2c5
Ryan Crookham <rcrookham@gmail.com>
2026-03-19 09:54:09 -0400

    Fix ESLint: suppress set-state-in-effect on hydration guard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
       : "/provider-dashboard";
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
